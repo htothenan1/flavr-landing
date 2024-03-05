064bd5c6195e1758d4ba87775aaa844a75f40b90
Hernan <hberisso1@gmail.com>
2024-03-05 16:14:29 -0500

    second pass

--- app/page.js
+++ app/page.js
@@ -1,24 +1,13 @@
-"use client"
-
-import { useState } from "react"
-import { Dialog } from "@headlessui/react"
-import {
-  Bars3Icon,
-  XMarkIcon,
-  ChatBubbleOvalLeftEllipsisIcon,
-  HeartIcon,
-  PencilSquareIcon,
-  TrashIcon,
-  BoltIcon,
-  LightBulbIcon,
-  ChartBarIcon,
-  SparklesIcon,
-} from "@heroicons/react/24/outline"
-import { ChevronRightIcon } from "@heroicons/react/20/solid"
 import Image from "next/image"
 import chefsHat from "../public/chefs_hat.png"
 import iphone from "../public/IPHONESCREENSHOT.png"
 import foodwaste from "../public/FOODWASTE.png"
+import {
+  BoltIcon,
+  PencilSquareIcon,
+  SparklesIcon,
+  LightBulbIcon,
+} from "@heroicons/react/24/outline"
 
 const navigation = [
   { name: "Features", href: "#features" },
@@ -35,7 +24,7 @@ const features = [
   {
     name: "Recipes On-Demand",
     description:
-      "Advanced recipe search tool, based on the items that you have. Find new and exciting ways to combine your foods.",
+      "Advanced recipe search tool, based on the items that you have. Find new and exciting ways to combine and consume your foods.",
     icon: PencilSquareIcon,
   },
   {
@@ -53,33 +42,18 @@ const features = [
 ]
 
 export default function Example() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-
   return (
     <div className="bg-white">
       <link rel="icon" href="/favicon.ico" sizes="any" />
-
       <header className="absolute inset-x-0 top-0 z-50">
         <nav
-          className="flex items-center justify-between p-6 lg:px-8"
+          className="flex items-center justify-between p-6"
           aria-label="Global"
         >
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <Image width={100} height={100} src={chefsHat} alt="" />
-            </a>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
+          <a href="#" className="-m-1.5 p-1.5">
+            <Image width={100} height={100} src={chefsHat} alt="" />
+          </a>
+          <div className="flex gap-x-12">
             {navigation.map((item) => (
               <a
                 key={item.name}
@@ -91,57 +65,6 @@ export default function Example() {
             ))}
           </div>
         </nav>
-        <Dialog
-          as="div"
-          className="lg:hidden"
-          open={mobileMenuOpen}
-          onClose={setMobileMenuOpen}
-        >
-          <div className="fixed inset-0 z-50" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
-              </a>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-                <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Log in
-                  </a>
-                </div>
-              </div>
-            </div>
-          </Dialog.Panel>
-        </Dialog>
       </header>
 
       <div className="relative isolate pt-14">
@@ -186,12 +109,14 @@ export default function Example() {
             </p>
             <div className="mt-10 flex items-center gap-x-6">
               <a
+                target="_blank"
                 href="https://testflight.apple.com/join/rkACgNHx"
                 className="rounded-md bg-gray-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Join our Beta! (iPhones only)
               </a>
               <a
+                target="_blank"
                 href="https://youtu.be/6eOLDqO_HO8?si=H9pxHYqK8AlyVZuR"
                 className="text-sm font-semibold leading-6 text-gray-900"
               >
@@ -249,12 +174,14 @@ export default function Example() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
+                target="_blank"
                 href="https://testflight.apple.com/join/rkACgNHx"
                 className="rounded-md bg-gray-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Join our Beta! (iPhones only)
               </a>
               <a
+                target="_blank"
                 href="https://refed.org/food-waste/climate-and-resources/"
                 className="text-sm font-semibold leading-6 text-gray-900"
               >

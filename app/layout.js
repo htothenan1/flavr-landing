21a70e22f756f4f7b0f99e28bd1bbd0e4e3fe168
Hernan <hberisso1@gmail.com>
2024-08-15 10:33:10 -0400

    change to flavrPro in meta layout

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "FlavrAi",
+  title: "FlavrPro",
   description: "Take control of your kitchen",
 }
 

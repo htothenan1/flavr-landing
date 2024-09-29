72a669ea6d3b043009b0f961c229d38c56758ff9
Hernan <hberisso1@gmail.com>
2024-09-29 07:26:33 -0400

    add favicon food box

--- app/layout.js
+++ app/layout.js
@@ -5,8 +5,8 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "FlavrPro",
-  description: "Take control of your kitchen",
+  title: "FeedLink",
+  description: "The FeedLink App",
 }
 
 export default function RootLayout({ children }) {
